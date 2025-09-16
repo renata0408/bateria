7f7dd8030a2b76b5356a1c4126718b3281cfdd9b
renata0408 <renata.cristovam@escola.pr.gov.br>
2025-09-16 12:19:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const sons = {
-    'A' : 'boom.wav'
+    "A" : "boom.wav",
+    "S" : "clap.wav",
+    "D" : "hihat.wav",
+    "F" : "kick.wav",
+    "G" : "openhat.wav",
+    "H" : "ride.wav",
+    "J" : "snare.wav",
+    "K" : "tink.wav",
+    "L" : "tom.wav",
 }
+
+//Função para criar uma div pra cada tecla
+const criarDiv = (sons) => {
+    div.classlist('key');
+    div.textcontent = texto
+}
+
+
+
+exibir(sons);
+document.getElementById('conteiner')
