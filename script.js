469807744610e54e47732e55e1579eb244e10232
renata0408 <renata.cristovam@escola.pr.gov.br>
2025-10-03 12:05:25 -0300

    a

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
+//Criação das constantes de som
 const sons = {
-    "A" : "boom.wav",
-    "S" : "clap.wav",
-    "D" : "hihat.wav",
-    "F" : "kick.wav",
-    "G" : "openhat.wav",
-    "H" : "ride.wav",
-    "J" : "snare.wav",
-    "K" : "tink.wav",
-    "L" : "tom.wav",
+    'A' : 'boom.wav',
+    'S' : 'clap.wav',
+    'D' : 'hithat.wav',
+    'F' : 'hithat.wav',
+    'G' : 'kick.wav',
+    'H' : 'openhat.wav',
+    'I' : 'ride.wav',
+    'J' : 'snare.wav',
+    'K' : 'tink.wav',
+    'L' : 'tom.wav',
+}
+//Função para criar uma div para cada tecla
+const criarDiv = (texto) => {
+    div.classList('key');
+    div.textContent = texto;
+    div.id =texto;
+    document.getElementById('container').append(div);
 }
 
-//Função para criar uma div pra cada tecla
-const criarDiv = (sons) => {
-    div.classlist('key');
-    div.textcontent = texto
-}
+//função para mostrar botões na tela
+const exibir = Object.keys(sons).forEach(criarDiv)
 
+
+const adicionarEfeito = (letra) => document.getElementById(letra).
+classList.toggle('active');
 
 
 exibir(sons);
-document.getElementById('conteiner')
+document.getElementById('container')
